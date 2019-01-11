f603ef3f01da410c9763cd299498e4917c8b8060
eLearning Team <elearning@lec.ac.uk>
2019-01-11 14:57:13 +0000

    Initial
    Working Draft version of Liftoff theme.

---
+++ classes/output/core_renderer.php
@@ -0,0 +1,85 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Core Renderer for the liftoff theme.
+ * Renderers to align Moodle's HTML with that expected by Bootstrap and the Boost parent theme.
+ *
+ * @package   theme_liftoff
+ * @copyright 2018 Leicester College (Developer: Alan Ball)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace theme_liftoff\output;
+
+use coding_exception;
+use html_writer;
+use tabobject;
+use tabtree;
+use custom_menu_item;
+use custom_menu;
+use block_contents;
+use navigation_node;
+use action_link;
+use stdClass;
+use moodle_url;
+use preferences_groups;
+use action_menu;
+use help_icon;
+use single_button;
+use paging_bar;
+use context_course;
+use pix_icon;
+
+defined('MOODLE_INTERNAL') || die;
+
+class core_renderer extends \theme_boost\output\core_renderer {
+
+    public function render_my_courses() {
+        global $CFG, $USER, $COURSE;
+
+        $menu = new custom_menu();
+
+        if (isloggedin() && !isguestuser() ) {
+            $branchtitle = "My Courses";
+            $branchlabel = $branchtitle;
+            $branchurl   = new moodle_url('/my/index.php');
+            $branchsort  = 9000;
+ 
+            $branch = $menu->add($branchlabel, $branchurl, $branchtitle, $branchsort);
+            if ($courses = enrol_get_my_courses(NULL, 'fullname ASC')) {
+                $branch->add('All of my courses...', new moodle_url('/my/index.php'), format_string('Dashboard'));
+                foreach ($courses as $course) {
+                    if ($course->visible){
+                        $branch->add(format_string($course->fullname), new moodle_url('/course/view.php?id='.$course->id), format_string($course->shortname));
+                    }
+                }
+            } else {
+                $noenrolments = 'No Enrolments Found';
+                $branch->add($noenrolments, new moodle_url('/'), $noenrolments);
+            }
+            
+        }
+        $content = '';
+        foreach ($menu->get_children() as $item) {
+            $context = $item->export_for_template($this);
+            $content .= $this->render_from_template('theme_liftoff/course_menu_items', $context);
+        }
+
+        return $content;
+    }
+
+}
---
+++ lib.php
@@ -0,0 +1,82 @@
+<?php
+ 
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Function Library for the liftoff theme.
+ *
+ * @package   theme_liftoff
+ * @copyright 2018 Leicester College (Developer: Alan Ball)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ 
+// This line protects the file from being accessed by a URL directly.                                                               
+defined('MOODLE_INTERNAL') || die();
+ 
+// We will add callbacks here as we add features to our theme.
+
+// Function to return the SCSS to prepend to our main SCSS for this theme.
+// Note the function name starts with the component name because this is a global function and we don't want namespace clashes.
+function theme_liftoff_get_pre_scss($theme) {
+    // Load the settings from the parent.                                                                                           
+    $theme = theme_config::load('boost');                                                                                           
+    // Call the parent themes get_pre_scss function.                                                                                
+    return theme_boost_get_pre_scss($theme);                         
+}
+
+// Function to return the SCSS to append to our main SCSS for this theme.
+// Note the function name starts with the component name because this is a global function and we don't want namespace clashes.
+function theme_liftoff_get_extra_scss($theme) {
+    // Load the settings from the parent.                                                                                           
+    $theme = theme_config::load('boost');                                                                                           
+    // Call the parent themes get_extra_scss function.                                                                                
+    return theme_boost_get_extra_scss($theme);                         
+}
+
+// This is the function that returns the SCSS source for the main file in our theme. We override the boost version because          
+// we want to allow presets uploaded to our own theme file area to be selected in the preset list.                                  
+
+function theme_liftoff_get_main_scss_content($theme) {                                                                                
+    global $CFG;                                                                                                                    
+ 
+    $scss = '';                                                                                                                     
+    $filename = !empty($theme->settings->preset) ? $theme->settings->preset : null;                                                 
+    $fs = get_file_storage();                                                                                                       
+ 
+    $context = context_system::instance();                                                                                          
+    if ($filename == 'default.scss') {                                                                                              
+        // We still load the default preset files directly from the boost theme. No sense in duplicating them.                      
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');                                        
+    } else if ($filename == 'plain.scss') {                                                                                         
+        // We still load the default preset files directly from the boost theme. No sense in duplicating them.                      
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/plain.scss');                                          
+ 
+    } else if ($filename && ($presetfile = $fs->get_file($context->id, 'theme_liftoff', 'preset', 0, '/', $filename))) {              
+        // This preset file was fetched from the file area for theme_photo and not theme_boost (see the line above).                
+        $scss .= $presetfile->get_content();                                                                                        
+    } else {                                                                                                                        
+        // Safety fallback - maybe new installs etc.                                                                                
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');                                        
+    }                                                                                                                                       
+ 
+    // Pre CSS - this is loaded AFTER any prescss from the setting but before the main scss.                                        
+    $pre = file_get_contents($CFG->dirroot . '/theme/photo/scss/pre.scss');                                                         
+    // Post CSS - this is loaded AFTER the main scss but before the extra scss from the setting.                                    
+    $post = file_get_contents($CFG->dirroot . '/theme/photo/scss/post.scss');                                                       
+ 
+    // Combine them together.                                                                                                       
+    return $pre . "\n" . $scss . "\n" . $post;                                                                                                                                   
+}
