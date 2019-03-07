44d1357b3707c0e979ef877ba6e30b41dce1491b
Alan Ball <aball@leicestercollege.ac.uk>
2019-03-07 15:16:35 +0000

    Refactoring

--- version.php
+++ version.php
@@ -27,8 +27,8 @@ defined('MOODLE_INTERNAL') || die();
  
 // This is the version of the plugin.                                                                                               
 $plugin->version = '2019030113';   
-$plugin->release = '1.0';                                                                                                 
- 
+$plugin->release = '1.0';                                                                                              
+   
 // This is the version of Moodle this plugin requires.                                                                              
 $plugin->requires = '2016070700';                                                                                                   
  
